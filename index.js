07f20b33a10ccab4be9c581c7ee7cebd8e011f5b
ZengJia <86162890+jiazengp@users.noreply.github.com>
2022-06-15 19:05:02 +0800

    pwa support

--- index.js
+++ index.js
@@ -268,3 +268,35 @@ cm.setCursor(START_CURSOR)
 cm.focus()
 
 readConfigs()
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+      .register('/pwa-examples/a2hs/sw.js')
+      .then(() => { console.log('Service Worker Registered'); });
+}
+
+// Code to handle install prompt on desktop
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+
+
+  addBtn.addEventListener('click', () => {
+      // Show the prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+              console.log('User accepted the A2HS prompt');
+          } else {
+              console.log('User dismissed the A2HS prompt');
+          }
+          deferredPrompt = null;
+      });
+  });
+});
